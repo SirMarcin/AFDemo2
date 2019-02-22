af5d132fb57644930c926ced53792d9b08b8f732
Marcin Webmaster <buczynski.programuje@gmail.com>
2019-02-22 13:51:14 +0100

    default system

--- AFDemo2/Program.cs
+++ AFDemo2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OSIsoft.AF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,31 @@ namespace AFDemo2
     {
         static void Main(string[] args)
         {
+            PISystems systems = new PISystems();
+
+            Console.WriteLine("Available systems:");
+            foreach (var system in systems)
+            {
+                Console.WriteLine("{0}", system.Name);
+            }
+
+            PISystem defaultSystem = systems.DefaultPISystem;
+            Console.WriteLine("Default system is {0}", defaultSystem);
+
+            defaultSystem.Connect();
+
+            Console.WriteLine("Existing databases in connected system:");
+            foreach (var database in defaultSystem.Databases)
+            {
+                Console.WriteLine("{0}",database.Name);
+            }
+
+            Console.WriteLine("Default database is: {0}", defaultSystem.Databases.DefaultDatabase.Name);
+
+            var db = defaultSystem.Databases.DefaultDatabase;
+            
+
+            Console.ReadKey();
         }
     }
 }
